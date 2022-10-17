fde428a4c065194c0062ddb09037702a3841e908
Barikuma <barikumamondelo@gmail.com>
2022-10-17 20:36:34 +0000

    printf update

--- printf.c
+++ printf.c
@@ -13,7 +13,6 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int len = 0, i = 0, tmp;
-
 	char next, current, *str;
 
 	va_start(args, format);
@@ -26,27 +25,28 @@ int _printf(const char *format, ...)
 		next = format[i + 1];
 
 		if (current == '%' && next == 'c')
-		{
 			_putchar((char)va_arg(args, int)), len++, i++;
-		}
+
 		else if (current == '%' && next == 's')
 		{
 			str = (char *)va_arg(args, char *);
-			_puts(str), len += _strlen(str), i++;
+			if (!str)
+			{
+				str = "(null)";
+				_puts(str), len += _strlen(str), i++;
+			}
+			else
+				_puts(str), len += _strlen(str), i++;
 		}
 		else if (current == '%' && next == '%')
-		{
 			_putchar('%'), len++, i++;
-		}
 		else if (current == '%' && (next == 'd' || next == 'i'))
 		{
 			tmp = va_arg(args, int);
 			_putint(tmp), len += intlen(tmp), i++;
 		}
 		else
-		{
 			_putchar(format[i]), len++;
-		}
 		i++;
 	}
 	va_end(args);
